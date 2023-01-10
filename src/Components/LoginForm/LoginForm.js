8da681d0a01077c9d88e488a4f9250476abad2f1
A. S. M Mustakim Rahman Siddique <mustakimpallab2434@gmail.com>
2023-01-11 03:39:53 +0600

    rong bodlay

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -353,7 +353,7 @@ const LoginForm = () => {
             type="button"
             data-mdb-ripple="true"
             data-mdb-ripple-color="light"
-            className="inline-block p-3 bg-green text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+            className="inline-block p-3 bg-green text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-900 hover:shadow-lg focus:bg-blue-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-900 active:shadow-lg transition duration-150 ease-in-out mx-1"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
